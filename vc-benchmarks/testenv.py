6e48b221bf47841403ceb0b8671ebbf079dea852
Mike Murphy <codermurph@sleepymurph.com>
2016-09-06 17:06:15 +0200

    Record command line as part of environment

--- vc-benchmarks/testenv.py
+++ vc-benchmarks/testenv.py
@@ -3,18 +3,21 @@
 import collections
 import datetime
 import socket
+import sys
 
 class TestEnv(collections.namedtuple(
         "TestEnv",
-        "hostname date")):
+        "hostname date commandline")):
 
     def pretty_print(self):
         print "hostname: %s" % self.hostname
         print "date: %s" % self.date.isoformat()
+        print "commandline: %s" % self.commandline
 
 def gather_environment_stats():
 
     return TestEnv(
             hostname = socket.gethostname(),
             date = datetime.datetime.utcnow(),
+            commandline = " ".join(sys.argv),
             )
