93e302f591d7d797e3a1bcc4cccff20145841b8e
Mike Murphy <codermurph@sleepymurph.com>
2016-09-12 18:55:05 +0200

    Refactor: Rename size variables

--- vc-benchmarks/increasing_file_size.py
+++ vc-benchmarks/increasing_file_size.py
@@ -34,20 +34,20 @@ def parse_args():
 
 class TestStats(collections.namedtuple(
         "TestStats",
-        "size create_time commit_time")):
+        "filebytes create_time commit_time")):
 
     columns = [
             ("magnitude", 9, "%9d"),
-            ("size", 20, "%20d"),
-            ("hsize", 8, "%8s"),
+            ("filebytes", 20, "%20d"),
+            ("filehsize", 9, "%9s"),
             ("create_time", 11, "%11.3f"),
             ("commit_time", 11, "%11.3f"),
         ]
 
     def __init__(self, **args):
         super(TestStats, self).__init__(args)
-        self.magnitude = math.frexp(self.size)[1]-1
-        self.hsize = hsize(self.size)
+        self.magnitude = math.frexp(self.filebytes)[1]-1
+        self.filehsize = filehsize(self.filebytes)
 
     @staticmethod
     def header():
@@ -68,7 +68,7 @@ class TestStats(collections.namedtuple(
         return "  ".join(stats)
 
 
-def hsize(num, suffix='B'):
+def filehsize(num, suffix='B'):
     for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
         if abs(num) < 1024.0:
             return "%3.1f%s%s" % (num, unit, suffix)
@@ -76,18 +76,18 @@ def hsize(num, suffix='B'):
     return "%.1f%s%s" % (num, 'Yi', suffix)
 
 
-def create_file(directory, name, size, data_gen='sparse'):
+def create_file(directory, name, filebytes, data_gen='sparse'):
     path = os.path.join(directory, name)
     with open(path, 'wb') as f:
         if data_gen=='sparse':
-            f.truncate(size)
+            f.truncate(filebytes)
         elif data_gen=='random':
-            f.write(os.urandom(size))
+            f.write(os.urandom(filebytes))
         else:
             raise "invalid data_gen strategy: " + data_gen
 
 
-def test_add_file(size, data_gen):
+def test_add_file(filebytes, data_gen):
     repodir = tempfile.mkdtemp(prefix='vcs_benchmark')
 
     try:
@@ -95,12 +95,12 @@ def test_add_file(size, data_gen):
         repo.init_repo()
 
         started_time = time.time()
-        create_file(repodir, "test_file", size, data_gen=data_gen)
+        create_file(repodir, "test_file", filebytes, data_gen=data_gen)
         created_time = time.time()
         repo.commit_file("test_file")
         committed_time = time.time()
         return TestStats(
-                    size = size,
+                    filebytes = filebytes,
                     commit_time = committed_time - created_time,
                     create_time = created_time - started_time,
                 )
