34ccbb4ad30aa81af7ada3b529c26a39046d3c47
Mike Murphy <codermurph@sleepymurph.com>
2016-09-13 21:02:18 +0200

    Record test version

--- vc-benchmarks/testenv.py
+++ vc-benchmarks/testenv.py
@@ -2,13 +2,14 @@
 
 import collections
 import datetime
+import os
 import socket
 import subprocess
 import sys
 
 class TestEnv(collections.namedtuple(
         "TestEnv",
-        "hostname date commandline memtotal memfree fsinfo")):
+        "hostname date commandline testversion memtotal memfree fsinfo")):
     pass
 
 def gather_environment_stats(dirs=[]):
@@ -16,6 +17,12 @@ def gather_environment_stats(dirs=[]):
     hostname = socket.gethostname()
     date = datetime.datetime.utcnow().isoformat()
     commandline = " ".join(sys.argv)
+
+    testversion = subprocess.check_output(
+                "git log -n1 --oneline -- .",
+                cwd = os.path.dirname(os.path.abspath(__file__)),
+                shell=True
+            ).strip()
 
     # Memory information
     meminfo = {}
@@ -37,6 +44,7 @@ def gather_environment_stats(dirs=[]):
             hostname = hostname,
             date = date,
             commandline = commandline,
+            testversion = testversion,
             memtotal = meminfo['MemTotal'],
             memfree = meminfo['MemFree'],
             fsinfo = fsinfo,
