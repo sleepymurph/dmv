29c9b5e0869aea7bbb34e460a85d6985f9a14b34
Mike Murphy <codermurph@sleepymurph.com>
2016-10-18 20:08:31 +0200

    Separate verification fields for commands

--- vc-benchmarks/increasing_number_of_files.py
+++ vc-benchmarks/increasing_number_of_files.py
@@ -49,33 +49,58 @@ def parse_args():
 
 class TrialStats:
 
+    cmdmax = CmdResults.max_width()
+    vermax = VerificationResults.max_width()
+
     columns = [
             Column("magnitude", "%9d", sample=0),
             Column("filecount", "%12d", sample=0),
             Column("totalbytes", "0x%010x", sample=0),
             Column("create_time", "%11.3f", sample=0),
+
             Column("c1_time", "%11.3f", sample=0),
             Column("c1_size", "0x%010x", sample=0),
+            Column("c1_cmd", "%s", max_w=cmdmax),
+            Column("c1_ver", "%s", max_w=vermax),
+            Column("c1_repo", "%s", max_w=vermax),
+
             Column("stat1_time", "%11.3f", sample=0),
+            Column("stat1_cmd", "%s", max_w=cmdmax),
             Column("stat2_time", "%11.3f", sample=0),
+            Column("stat2_cmd", "%s", max_w=cmdmax),
+
             Column("c2_time", "%11.3f", sample=0),
             Column("c2_size", "0x%010x", sample=0),
+            Column("c2_cmd", "%s", max_w=cmdmax),
+            Column("c2_ver", "%s", max_w=vermax),
+            Column("c2_repo", "%s", max_w=vermax),
+
             Column("cleanup_time", "%11.3f", sample=0),
-            Column("errors", "%6s", sample=False),
         ]
 
     def __init__(self, **args):
         self.filecount = 0
         self.eachbytes = 0
         self.create_time = 0
+
         self.c1_time = 0
         self.c1_size = 0
+        self.c1_cmd = CmdResults.value('no_exec')
+        self.c1_ver = VerificationResults.value('no_ver')
+        self.c1_repo = VerificationResults.value('no_ver')
+
         self.stat1_time = 0
+        self.stat1_cmd = CmdResults.value('no_exec')
         self.stat2_time = 0
+        self.stat2_cmd = CmdResults.value('no_exec')
+
         self.c2_time = 0
         self.c2_size = 0
+        self.c2_cmd = CmdResults.value('no_exec')
+        self.c2_ver = VerificationResults.value('no_ver')
+        self.c2_repo = VerificationResults.value('no_ver')
+
         self.cleanup_time = 0
-        self.errors = False
 
     def calculate_columns(self):
         self.magnitude = math.log10(self.filecount)
@@ -92,47 +117,43 @@ def run_trial(vcsclass, numfiles, filebytes, data_gen, tmpdir="/tmp"):
         repo = vcsclass(repodir)
         repo.init_repo()
 
-        with StopWatch(ts, "create_time"):
+        with \
+                StopWatch(ts, "create_time"):
             create_many_files(
                     repodir, numfiles, filebytes,
                     prefix="many_files_dir", data_gen=data_gen)
 
-        try:
-            with StopWatch(ts, "c1_time"):
-                repo.start_tracking_file("many_files_dir")
-                repo.commit_file("many_files_dir")
-        except CallFailedError as e:
-            log(e)
-            ts.errors = True
+        with \
+                RepoVerifier(repo, ts, 'c1_repo'), \
+                CommitVerifier(repo, obj=ts, attr='c1_ver'), \
+                CmdResult(ts, 'c1_cmd'), \
+                StopWatch(ts, "c1_time"):
+            repo.start_tracking_file("many_files_dir")
+            repo.commit_file("many_files_dir")
         ts.c1_size = repo.check_total_size()
 
-        try:
-            with StopWatch(ts, 'stat1_time'):
-                repo.check_status("many_files_dir")
-        except CallFailedError as e:
-            log(e)
-            ts.errors = True
+        with \
+                CmdResult(ts, 'stat1_cmd'), \
+                StopWatch(ts, 'stat1_time'):
+            repo.check_status("many_files_dir")
 
         update_many_files(repodir, "many_files_dir", every_nth_file=16)
 
-        try:
-            with StopWatch(ts, 'stat2_time'):
-                repo.check_status("many_files_dir")
-        except CallFailedError as e:
-            log(e)
-            errors = True
+        with \
+                CmdResult(ts, 'stat2_cmd'), \
+                StopWatch(ts, 'stat2_time'):
+            repo.check_status("many_files_dir")
 
-        try:
-            with StopWatch(ts, 'c2_time'):
-                repo.commit_file("many_files_dir")
-        except CallFailedError as e:
-            log(e)
-            ts.errors = True
+        with \
+                RepoVerifier(repo, ts, 'c2_repo'), \
+                CommitVerifier(repo, obj=ts, attr='c2_ver'), \
+                CmdResult(ts, 'c2_cmd'), \
+                StopWatch(ts, 'c2_time'):
+            repo.commit_file("many_files_dir")
         ts.c2_size = repo.check_total_size()
 
     except Exception as e:
-        ts.errors = True
-        raise e
+        log(repr(e))
 
     finally:
         log("Cleaning up trial files...")
