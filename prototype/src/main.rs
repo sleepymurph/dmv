5f714b8b1f7430297fd2e4440d4a19e185eaebe8
Mike Murphy <codermurph@sleepymurph.com>
2016-10-31 12:25:29 +0100

    Bring in clap-rs for argument parsing

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -1,3 +1,27 @@
+extern crate clap;
+use clap::{App, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Store Prototype")
+        .subcommand(SubCommand::with_name("commit")
+                    .arg(Arg::with_name("file"))
+                    )
+        .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("commit") {
+        println!("commit!");
+        if let Some(filename) = matches.value_of("file") {
+            println!("filename: {}", filename)
+        } else {
+            println!("no filename given")
+        }
+    } else {
+        println!("not commit.");
+    }
+
+}
+
+#[test]
+fn meta_test() {
+    assert!(true);
 }
