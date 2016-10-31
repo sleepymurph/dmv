1994784fa8a4ab83c189e2c46a3abaab1913bafb
Mike Murphy <codermurph@sleepymurph.com>
2016-10-31 17:36:09 +0100

    Refactor a lib/cli split

---
+++ prototype/src/lib.rs
@@ -0,0 +1,47 @@
+extern crate clap;
+extern crate crypto;
+
+pub mod addfile {
+
+    use std::error::Error;
+    use std::fs::File;
+    use std::io::Read;
+    use std::path::Path;
+
+    use clap::{App, Arg, ArgMatches, SubCommand};
+    use crypto::digest::Digest;
+    use crypto::sha1::Sha1;
+
+    pub fn subcommand<'a>() -> App<'a, 'a> {
+        SubCommand::with_name("commit").arg(Arg::with_name("file"))
+    }
+
+    pub fn subcommand_match(matches: &ArgMatches) {
+        println!("commit!");
+        if let Some(filename) = matches.value_of("file") {
+            println!("filename: {}", filename);
+            let path = Path::new(filename);
+            let mut file = match File::open(&path) {
+                Err(why) => panic!("Couldn't open {}: {}",
+                                   path.display(), why.description()),
+                Ok(file) => file,
+            };
+
+            let mut buffer: [u8; 1024] = [0; 1024];
+            let mut digest = Sha1::new();
+            loop {
+                match file.read(&mut buffer) {
+                    Err(why) => panic!("Error reading {}: {}",
+                                       path.display(), why.description()),
+                    Ok(0) => break,
+                    Ok(bytes) => {
+                        digest.input(&buffer[0..bytes]);
+                    }
+                }
+            }
+            println!("{}", digest.result_str());
+        } else {
+            println!("no filename given");
+        }
+    }
+}
--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -1,59 +1,21 @@
-use std::error::Error;
-use std::fs::File;
-use std::io::Read;
-use std::path::Path;
-
 extern crate clap;
-use clap::{App, Arg, SubCommand};
+extern crate prototype;
 
-extern crate crypto;
-use crypto::digest::Digest;
-use crypto::sha1::Sha1;
+use std::io::Write;
+
+use clap::App;
+
+use prototype::*;
 
 fn main() {
-    let matches = App::new("Store Prototype")
-        .subcommand(SubCommand::with_name("commit")
-                    .arg(Arg::with_name("file"))
-                    )
+    let app_m = App::new("Store Prototype")
+        .subcommand(addfile::subcommand())
         .get_matches();
 
-    if let Some(matches) = matches.subcommand_matches("commit") {
-        println!("commit!");
-        if let Some(filename) = matches.value_of("file") {
-            println!("filename: {}", filename);
-            let path = Path::new(filename);
-            let mut file = match File::open(&path) {
-                Err(why) => panic!("Couldn't open {}: {}",
-                                   path.display(), why.description()),
-                Ok(file) => file,
-            };
-
-            let mut buffer: [u8; 1024] = [0; 1024];
-            let mut digest = Sha1::new();
-            loop {
-                match file.read(&mut buffer) {
-                    Err(why) => panic!("Error reading {}: {}",
-                                       path.display(), why.description()),
-                    Ok(0) => break,
-                    Ok(bytes) => {
-                        digest.input(&buffer[0..bytes]);
-                    }
-                }
-            };
-            println!("{}", digest.result_str());
-        } else {
-            println!("no filename given");
-        };
-    } else {
-        println!("not commit.");
-    };
-
-}
-
-#[test]
-fn test_sha1_empty() {
-    let mut digest = Sha1::new();
-    digest.input_str("");
-    let outstr = digest.result_str();
-    assert!(outstr == "da39a3ee5e6b4b0d3255bfef95601890afd80709");
+    match app_m.subcommand() {
+        ("commit", Some(sub_m)) => addfile::subcommand_match(sub_m),
+        _ => {
+            writeln!(std::io::stderr(), "{}", app_m.usage()).unwrap();
+        }
+    }
 }
