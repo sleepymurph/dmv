e3dd592fc58a4d81308da85fc0a35608c6a84c24
Mike Murphy <codermurph@sleepymurph.com>
2016-11-02 16:12:47 +0100

    Refactor into a repository object

--- prototype/src/lib.rs
+++ prototype/src/lib.rs
@@ -10,6 +10,37 @@ use clap::{App, Arg, ArgMatches, SubCommand};
 use crypto::digest::Digest;
 use crypto::sha1::Sha1;
 
+pub trait Repository {
+    fn hash_object(&self, path: &Path) -> Result<String, std::io::Error>;
+}
+
+pub struct OnDiskRepository;
+
+impl Repository for OnDiskRepository {
+    fn hash_object(&self, path: &Path) -> Result<String, std::io::Error> {
+
+        let mut file = try!(File::open(&path));
+
+        let mut buffer: [u8; 1024] = [0; 1024];
+        let mut digest = Sha1::new();
+
+        loop {
+            match file.read(&mut buffer) {
+                Err(why) => {
+                    panic!("Error reading {}: {}",
+                           path.display(),
+                           why.description())
+                }
+                Ok(0) => break,
+                Ok(bytes) => {
+                    digest.input(&buffer[0..bytes]);
+                }
+            }
+        }
+        Ok(digest.result_str())
+    }
+}
+
 pub trait PrototypeCommand {
     fn name() -> &'static str;
     fn subcommand<'a, 'b>() -> App<'a, 'b>;
@@ -29,32 +60,14 @@ impl PrototypeCommand for HashObjectCommand {
 
     fn subcommand_match(matches: &ArgMatches) {
         if let Some(filename) = matches.value_of("file") {
+            let repo = OnDiskRepository {};
             let path = Path::new(filename);
-            let mut file = match File::open(&path) {
+            match repo.hash_object(path) {
+                Ok(hash) => println!("{}", hash),
                 Err(why) => {
-                    panic!("Couldn't open {}: {}",
-                           path.display(),
-                           why.description())
-                }
-                Ok(file) => file,
-            };
-
-            let mut buffer: [u8; 1024] = [0; 1024];
-            let mut digest = Sha1::new();
-            loop {
-                match file.read(&mut buffer) {
-                    Err(why) => {
-                        panic!("Error reading {}: {}",
-                               path.display(),
-                               why.description())
-                    }
-                    Ok(0) => break,
-                    Ok(bytes) => {
-                        digest.input(&buffer[0..bytes]);
-                    }
+                    panic!("Could not open {}: {}", filename, why.description())
                 }
             }
-            println!("{}", digest.result_str());
         } else {
             println!("no filename given");
         }
