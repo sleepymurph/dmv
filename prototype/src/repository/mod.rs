f06a4882bb352bcc1e40b125aa1d83040e675445
Mike Murphy <codermurph@sleepymurph.com>
2016-11-04 19:54:33 +0100

    Add temporary test to attempt to test traits

--- prototype/src/repository/mod.rs
+++ prototype/src/repository/mod.rs
@@ -19,3 +19,62 @@ pub trait Repository<'a> {
     fn read_object(&mut self, key: &ObjectKey) -> io::Result<Self::ReadType>;
     fn add_object(&'a mut self) -> io::Result<Self::IncomingType>;
 }
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+    use std::io::{Read, Write};
+    use std::io;
+
+    /*
+    fn run_repository_trait_tests<'r, F, R>(create_temp_repo: F)
+        where F: Fn() -> R,
+              R: Repository<'r> + 'r
+    {
+        let mut repo = create_temp_repo();
+        let data = "here be content";
+        let key = "9cac8e6ad1da3212c89b73fdbb2302180123b9ca";
+        {
+            let mut incoming = repo.add_object().expect("open incoming");
+            incoming.write(data.as_bytes()).expect("write to incoming");
+            incoming.flush().expect("flush incoming");
+            incoming.set_key(key).expect("set key");
+        }
+        assert_eq!(repo.has_object(key), true);
+
+        let mut reader = repo.read_object(key).expect("open saved object");
+        let mut read_data = String::new();
+        reader.read_to_string(&mut read_data).expect("read saved object");
+        assert_eq!(read_data, data);
+    }
+    */
+
+    pub trait Foo {
+        fn new() -> Self;
+        fn party(&self) -> String;
+    }
+
+    struct Bar {}
+    impl Foo for Bar {
+        fn new() -> Bar {
+            Bar {}
+        }
+        fn party(&self) -> String {
+            "party!".into()
+        }
+    }
+
+    fn do_foo_tests<F,T>(create: F)
+        where F: Fn()->T,
+              T: Foo
+    {
+        let mut foo = create();
+        assert_eq!(foo.party(), "party!".to_string());
+    }
+
+    #[test]
+    fn test_factory() {
+        do_foo_tests(Bar::new);
+    }
+}
