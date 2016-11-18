3e2cf0b6879f6988aa6574c49b548530530c7fb6
Mike Murphy <codermurph@sleepymurph.com>
2016-11-18 11:59:06 +0100

    Settle on Object enum structure, write HashWriter

--- prototype/src/dag2.rs
+++ prototype/src/dag2.rs
@@ -1,7 +1,18 @@
 //! Implementation of the directed acyclic graph (DAG)
 
-use std::fmt;
+extern crate crypto;
+use self::crypto::digest::Digest;
+
 use std::convert;
+use std::error;
+use std::fmt;
+use std::io;
+
+/// Type used for sizing and seeking in objects
+type ObjectSize = u64;
+
+/// Hash type
+type Hasher = crypto::sha1::Sha1;
 
 /// Object key size in bits
 pub const KEY_SIZE_BITS: usize = 160;
@@ -23,6 +34,11 @@ pub struct ObjectKey {
 }
 
 impl ObjectKey {
+    /// Creates a new all-zero key
+    fn zero() -> Self {
+        ObjectKey { hash: [0; KEY_SIZE_BYTES] }
+    }
+
     pub fn from_hex(hexstr: &str) -> Result<Self, DagError> {
         if hexstr.len() != KEY_SIZE_BYTES * 2 {
             return Err(DagError::ParseKey { bad_key: hexstr.to_string() });
@@ -86,9 +102,93 @@ impl convert::From<ObjectKey> for String {
     }
 }
 
+
+#[derive(Clone,Eq,PartialEq,Ord,PartialOrd,Hash,Debug)]
+pub enum Object {
+    /// Blobs
+    ///
+    /// Blobs are a special case because often when dealing with the DAG we
+    /// don't need to read in the actual data. So we have an Option for the
+    /// binary data, which represents whether the data has been loaded or not.
+    /// It will not be read in by default, but it will be necessary when writing
+    /// out, in order to finish the write and compute the hash.
+    ///
+    /// Blobs are assumed to be able to fit in memory because of the way that
+    /// large files are broken into chunks when stored. So it should be safe to
+    /// use a `Vec<u8>` to hold the contents.
+    Blob {
+        size: ObjectSize,
+        content: Option<Vec<u8>>,
+    },
+    ChunkedBlob {
+        // TODO: list of chunk mappings
+        size: ObjectSize,
+        total_blob_size: ObjectSize,
+    },
+    Tree {
+        // TODO: list of file mappings
+        size: ObjectSize,
+    },
+    Commit {
+        // TODO: parents, message, author, etc.
+        size: ObjectSize,
+        tree: ObjectKey,
+    },
+}
+
+impl Object {
+    /// Write object to output
+    ///
+    /// When writing a `Blob`, the content must be available, or else the write
+    /// will not be able to finish to get the hash of the object.
+    pub fn write_to(&self, writer: &mut io::Write) -> io::Result<ObjectKey> {
+        unimplemented!()
+    }
+
+    pub fn read_from<R: io::BufRead>(reader: &mut R)
+                                     -> Result<Object, DagError> {
+        unimplemented!()
+    }
+}
+
+/// Wraps an existing writer and computes a hash of the bytes going into it
+pub struct HashWriter<W: io::Write> {
+    writer: W,
+    hasher: Hasher,
+}
+
+impl<W: io::Write> HashWriter<W> {
+    fn wrap(writer: W) -> Self {
+        HashWriter {
+            writer: writer,
+            hasher: Hasher::new(),
+        }
+    }
+
+    fn hash(&mut self) -> ObjectKey {
+        let mut key = ObjectKey::zero();
+        self.hasher.result(&mut key.hash);
+        key
+    }
+}
+
+impl<W: io::Write> io::Write for HashWriter<W> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let count = try!(self.writer.write(buf));
+        self.hasher.input(&buf[0..count]);
+        Ok(count)
+    }
+    fn flush(&mut self) -> io::Result<()> {
+        self.writer.flush()
+    }
+}
+
+
 #[cfg(test)]
 mod test {
     use super::*;
+
+    use std::io::Write;
 
     #[test]
     fn test_key_hex_conversions() {
@@ -122,5 +222,23 @@ mod test {
                        "{}",
                        desc);
         }
+    }
+
+    #[test]
+    fn test_hash_write() {
+        let input = b"Hello world!";
+        let expected_hash =
+            ObjectKey::from_hex("d3486ae9136e7856bc42212385ea797094475802")
+                .unwrap();
+
+        let mut output: Vec<u8> = Vec::new();
+        {
+            let mut hasher = HashWriter::wrap(&mut output);
+            hasher.write(input).expect("write input");
+            assert_eq!(hasher.hash(), expected_hash);
+            hasher.flush().expect("flush hash writer");
+        }
+
+        assert_eq!(output, input);
     }
 }
