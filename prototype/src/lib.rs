171e03bd39fe73ffecadca8ce55df5e07ceb1e84
Mike Murphy <codermurph@sleepymurph.com>
2016-11-22 19:32:06 +0100

    Store chunked file

--- prototype/src/lib.rs
+++ prototype/src/lib.rs
@@ -27,12 +27,11 @@ pub mod repo {
 
     use std::fs;
     use std::io;
-    use std::io::Read;
     use std::path;
 
     use dag;
-    use dag::Object;
     use objectstore;
+    use rollinghash;
 
     pub struct WorkDir {
         path: path::PathBuf,
@@ -52,20 +51,45 @@ pub mod repo {
             self.workdir_path().map(|p| p.join(addition))
         }
 
+        pub fn store_object<O: dag::Object>(&mut self,
+                                            obj: &O)
+                                            -> io::Result<dag::ObjectKey> {
+            let mut incoming = try!(self.objectstore.new_object());
+            let key = try!(obj.write_to(&mut incoming));
+            try!(self.objectstore.save_object(key, incoming));
+            Ok(key)
+        }
+
         pub fn store_file(&mut self,
                           path: &path::Path)
                           -> io::Result<dag::ObjectKey> {
 
-            let mut file = try!(fs::File::open(path));
-            let mut contents:Vec<u8> = Vec::new();
-            try!(file.read_to_end(&mut contents));
+            let file = try!(fs::File::open(path));
+            let file = io::BufReader::new(file);
 
-            let blob = dag::Blob::from_vec(contents);
+            let mut chunker = rollinghash::ChunkReader::wrap(file);
+            let chunk1 = chunker.next();
+            let chunk2 = chunker.next();
 
-            let mut incoming = try!(self.objectstore.new_object());
-            let key = try!(blob.write_to(&mut incoming));
-            try!(self.objectstore.save_object(key, incoming));
-            Ok(key)
+            match (chunk1, chunk2) {
+                (None, None) => unimplemented!(),
+                (Some(v1), None) => {
+                    let blob = dag::Blob::from_vec(v1?);
+                    self.store_object(&blob)
+                }
+                (Some(v1), Some(v2)) => {
+                    let mut chunkedblob = dag::ChunkedBlob::new();
+
+                    for chunk in vec![v1, v2].into_iter().chain(chunker) {
+                        let blob = dag::Blob::from_vec(chunk?);
+                        let key = try!(self.store_object(&blob));
+                        chunkedblob.add_chunk(blob.size(), key);
+                    }
+
+                    self.store_object(&chunkedblob)
+                }
+                (None, Some(_)) => unreachable!(),
+            }
         }
     }
 
@@ -80,6 +104,7 @@ pub mod repo {
         use dag;
         use dag::Object;
         use objectstore;
+        use rollinghash;
         use testutil;
 
         use super::*;
@@ -112,14 +137,48 @@ pub mod repo {
 
             let hash = repo.store_file(&filepath).unwrap();
 
-            let blob = repo.objectstore.read_object(&hash).unwrap();
-            let mut blob = io::BufReader::new(blob);
-            let header = dag::ObjectHeader::read_from(&mut blob).unwrap();
+            let obj = repo.objectstore.read_object(&hash).unwrap();
+            let mut obj = io::BufReader::new(obj);
 
+            let header = dag::ObjectHeader::read_from(&mut obj).unwrap();
             assert_eq!(header.object_type, dag::ObjectType::Blob);
 
-            let blob = dag::Blob::read_from(&mut blob).unwrap();
+            let blob = dag::Blob::read_from(&mut obj).unwrap();
             assert_eq!(String::from_utf8(blob.content).unwrap(), "foo");
+        }
+
+        #[test]
+        fn test_store_file_chunked() {
+            let (_temp, mut repo) = create_temp_repository().unwrap();
+            let filepath = repo.workdir_join("foo").unwrap();
+            let filesize = 3 * rollinghash::CHUNK_TARGET_SIZE as u64;
+
+            let mut rng = testutil::RandBytes::new();
+            rng.write_file(&filepath, filesize)
+                .unwrap();
+
+            let hash = repo.store_file(&filepath).unwrap();
+
+            let obj = repo.objectstore.read_object(&hash).unwrap();
+            let mut obj = io::BufReader::new(obj);
+            let header = dag::ObjectHeader::read_from(&mut obj).unwrap();
+
+            assert_eq!(header.object_type, dag::ObjectType::ChunkedBlob);
+
+            let chunked = dag::ChunkedBlob::read_from(&mut obj).unwrap();
+            assert_eq!(chunked.total_size, filesize);
+            assert_eq!(chunked.chunks.len(), 5);
+
+            for chunkrecord in chunked.chunks {
+                let obj =
+                    repo.objectstore.read_object(&chunkrecord.hash).unwrap();
+                let mut obj = io::BufReader::new(obj);
+                let header = dag::ObjectHeader::read_from(&mut obj).unwrap();
+                assert_eq!(header.object_type, dag::ObjectType::Blob);
+
+                let blob = dag::Blob::read_from(&mut obj).unwrap();
+                assert_eq!(blob.size(), chunkrecord.size);
+            }
         }
     }
 }
