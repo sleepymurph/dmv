5fff6a649b322da331cdc068350d2d5d06898c27
Mike Murphy <codermurph@sleepymurph.com>
2016-12-16 16:52:27 +0100

    Make ObjectStore a child of WorkDir, remove Repo

--- prototype/src/repo.rs
+++ prototype/src/repo.rs
@@ -10,31 +10,23 @@ pub struct WorkDir {
     path: path::PathBuf,
     current_branch: Option<dag::ObjectKey>,
     cache: cache::FileCache,
-}
-
-pub struct Repo {
-    pub objectstore: objectstore::ObjectStore,
-    pub workdir: WorkDir,
+    objectstore: objectstore::ObjectStore,
 }
 
 impl WorkDir {
     pub fn path(&self) -> &path::Path {
         &self.path
     }
-}
 
-impl Repo {
     pub fn check_status(&self) -> io::Result<status::DirStatus> {
-        let meta = try!(self.workdir.path.metadata());
-        let status =
-            try!(self.check_status_path(self.workdir.path(),
-                                        &meta,
-                                        self.workdir.current_branch.as_ref()));
+        let meta = try!(self.path.metadata());
+        let status = try!(self.check_status_path(self.path(),
+                                                 &meta,
+                                                 self.current_branch.as_ref()));
         match status {
             status::PathStatus::ModifiedDir { status, .. } => Ok(status),
             _ => {
-                panic!("Working directory is not a directory: {:?}",
-                       &self.workdir.path)
+                panic!("Working directory is not a directory: {:?}", &self.path)
             }
         }
     }
@@ -81,7 +73,7 @@ impl Repo {
          relpath: &P)
          -> io::Result<dag::ObjectKey> {
 
-        let abspath = self.workdir.path.join(relpath);
+        let abspath = self.path.join(relpath);
         let mut tree = dag::Tree::new();
 
         for entry in try!(fs::read_dir(&abspath)) {
@@ -126,35 +118,32 @@ mod test {
     use super::*;
     use testutil;
 
-    fn create_temp_repository() -> io::Result<(tempdir::TempDir, Repo)> {
+    fn create_temp_repository() -> io::Result<(tempdir::TempDir, WorkDir)> {
         let wd_temp = try!(tempdir::TempDir::new_in("/dev/shm",
                                                     "test_repository"));
         let wd_path = wd_temp.path().to_path_buf();
         try!(fs::create_dir_all(&wd_path));
-        let wd = WorkDir {
-            path: wd_path.clone(),
-            current_branch: None,
-            cache: cache::FileCache::new(),
-        };
 
         let os_path = wd_path.join(".prototype");
         let os = objectstore::ObjectStore::new(&os_path);
         try!(os.init());
 
-        let repo = Repo {
+        let wd = WorkDir {
+            path: wd_path.clone(),
+            current_branch: None,
+            cache: cache::FileCache::new(),
             objectstore: os,
-            workdir: wd,
         };
 
-        Ok((wd_temp, repo))
+        Ok((wd_temp, wd))
     }
 
     #[test]
     fn test_store_directory() {
-        let (_temp, mut repo) = create_temp_repository().unwrap();
+        let (_temp, mut workdir) = create_temp_repository().unwrap();
         let mut rng = testutil::RandBytes::new();
 
-        let wd_path = repo.workdir.path().to_owned();
+        let wd_path = workdir.path().to_owned();
 
         testutil::write_str_file(&wd_path.join("foo"), "foo").unwrap();
         testutil::write_str_file(&wd_path.join("bar"), "bar").unwrap();
@@ -162,9 +151,9 @@ mod test {
         let filesize = 3 * rollinghash::CHUNK_TARGET_SIZE as u64;
         rng.write_file(&wd_path.join("baz"), filesize).unwrap();
 
-        let hash = repo.store_directory(&".").unwrap();
+        let hash = workdir.store_directory(&".").unwrap();
 
-        let obj = repo.objectstore.read_object(&hash).unwrap();
+        let obj = workdir.objectstore.read_object(&hash).unwrap();
         let mut obj = io::BufReader::new(obj);
         let header = dag::ObjectHeader::read_from(&mut obj).unwrap();
 
@@ -180,10 +169,10 @@ mod test {
 
     #[test]
     fn test_check_status_no_cache() {
-        let (_temp, repo) = create_temp_repository().unwrap();
+        let (_temp, workdir) = create_temp_repository().unwrap();
         let mut rng = testutil::RandBytes::new();
 
-        let wd_path = repo.workdir.path();
+        let wd_path = workdir.path();
 
         testutil::write_str_file(&wd_path.join("foo"), "foo").unwrap();
         testutil::write_str_file(&wd_path.join("bar"), "bar").unwrap();
@@ -194,7 +183,7 @@ mod test {
         testutil::write_str_file(&wd_path.join("sub/x"), "new x").unwrap();
         testutil::write_str_file(&wd_path.join("sub/y"), "new y").unwrap();
 
-        let status = repo.check_status().unwrap();
+        let status = workdir.check_status().unwrap();
 
         assert!(status.is_modified());
         assert_eq!(status.to_hash_total_size(), 46096);
