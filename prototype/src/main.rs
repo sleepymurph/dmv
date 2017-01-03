5d77b9da14fd1b097dc49a790fc3235ac7b4e8bd
Mike Murphy <codermurph@sleepymurph.com>
2017-01-03 14:08:02 +0200

    Begin a CLI

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+#[macro_use]
+extern crate clap;
+
+fn main() {
+
+    println!("Parsing!");
+
+    let arg_yaml = load_yaml!("cli.yaml");
+    let argmatch = clap::App::from_yaml(arg_yaml).get_matches();
+
+    if let Some(submatch) = argmatch.subcommand_matches("hash-object") {
+        println!("hash-object! - filepath: {}",
+                 submatch.value_of("filepath").unwrap());
+    } else {
+        println!("unknown!");
+    }
+}
