8e0fabdbcd8f671ffeb14bd69e245db52c28c7bd
Mike Murphy <codermurph@sleepymurph.com>
2017-05-15 12:57:37 +0200

    Show only Git rev in version, not msg

--- build.rs
+++ build.rs
@@ -8,7 +8,7 @@ fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
 
     // Save git version
-    let output = Command::new("git").arg("log").arg("-n1").arg("--oneline")
+    let output = Command::new("git").arg("rev-parse").arg("--short").arg("HEAD")
         .output().expect("could not get current Git version");
 
     let stdout = String::from_utf8(output.stdout).unwrap();
